624e5e3f34688708b74b5cff9bcbb9b949f44b51
sunil <sunil@PredatorOnHunt>
2020-08-23 18:56:49 +0530

    Patterns

--- JigsawTestArifactId/src/main/java/FirstPackage/First.java
+++ JigsawTestArifactId/src/main/java/FirstPackage/First.java
@@ -3,7 +3,29 @@ package FirstPackage;
 import java.util.Scanner;
 
 public class First {
+public static void reversePatterns() {
 
+		
+		Scanner input = new Scanner(System.in);
+		int givenInput=input.nextInt();
+		int s=givenInput-1;
+		int st=1;
+		while(givenInput>0) {
+			
+			
+			for(int spaces=0;spaces<s;spaces++) {
+				System.out.print(" ");
+				
+			}
+			for(int stars=0;stars<st;stars++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			s=s-1;
+			st=st+1;
+			givenInput-=1;
+		}
+}
 	public static void main(String[] args) {
 		System.out.println("This is a development");
 		Scanner input = new Scanner(System.in);
